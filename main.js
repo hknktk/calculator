ba735e0cca05d78ecc154e66787b4388ee84d084
hknktk <75674444+hknktk@users.noreply.github.com>
2021-04-15 14:31:09 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
 {
+
+  let output = document.querySelector('#output');
+
   const number_buttons = document.querySelectorAll('.number_button');
 
+  
+
   [...number_buttons].forEach((number_buttons) => {
+    let number_value = number_buttons.innerHTML;
+    
     number_buttons.addEventListener('click', () => {
-      console.log(number_buttons.innerHTML);
+      if(output.textContent === ""){
+        output.textContent = number_value;
+      }else{
+        output.textContent += number_value;
+      }
+      
+      console.log(output.textContent);
     })
   })
 
-  let output = document.querySelector('#output');
-  console.log(output.innerHTML.trim());
+  const clear_button = document.querySelector('#clear');
+  clear_button.addEventListener('click', () => {
+    output.textContent = "0";
+
+    console.log(output.textContent);
+  })
+  
+
+  const save_button = document.querySelector('#save');
+  save_button.addEventListener('click', () =>{
+    output.classList.toggle('once_save');
+    if (output.className !== "output"){
+      output.textContent = "0";
+    }
+  })
+
+ 
 }
